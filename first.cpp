9bc8d10694c50764030e606ff0ee26b58b879f6c
kishan mandal <jhabalram661@gmail.com>
2025-12-11 13:40:47 +0530

    this is a a new firsther

--- first.cpp
+++ first.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int arr[5]= {10, 38, 39, 29, 30};
-    int n=5;
-    // sorting (bubble sort )
-    for(int i=0 ; i<n-1; i++){
+int main() {
+    int arr[5] ={ 40, 49 , 9 , 39 , 38};
+    int n = 5; 
+    for (int i=0; i<n-1; i++ ){
     for (int j=0; j<n-i-1; j++){
-        if (arr[j] > arr[j+1]){
-            int temp=arr[j];
-            arr[j] =arr[j+1];
-            arr[j+1] = temp;
-        }
-     }
+    if (arr[j] >arr[j+1]){
+        int temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1] = temp;
     }
-cout <<" sorted array ";
-for (int i=0; i<n ; i++){
-    cout <<arr[i]<<" ";
-}
-return 0;
-
+    }
+    }
+    return 0; 
 }
