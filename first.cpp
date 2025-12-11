9d457729917424b8f0ad14271de1721288a2e0ab
kishan mandal <jhabalram661@gmail.com>
2025-12-11 13:24:44 +0530

    this is a sotin a array

--- first.cpp
+++ first.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "this is a cpp file";
-    return 0;
+    int arr[5]= {10, 38, 39, 29, 30};
+    int n=5;
+    // sorting (bubble sort )
+    for(int i=0 ; i<n-1; i++){
+    for (int j=0; j<n-i-1; j++){
+        if (arr[j] > arr[j+1]){
+            int temp=arr[j];
+            arr[j] =arr[j+1];
+            arr[j+1] = temp;
+        }
+     }
+    }
+cout <<" sorted array ";
+for (int i=0; i<n ; i++){
+    cout <<arr[i]<<" ";
+}
+return 0;
+
 }
