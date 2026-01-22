e8bfaebf3d3e2a61a45ee6bfca73c1b7c6500dc0
kishan mandal <jhabalram661@gmail.com>
2026-01-22 17:45:15 +0530

    this is a code of sum, max, min , revers , in c++

--- first.cpp
+++ first.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
-using namespace std;
-int main() {
-    int arr[5] ={ 40, 49 , 9 , 39 , 38};
-    int n = 5; 
-    for (int i=0; i<n-1; i++ ){
-    for (int j=0; j<n-i-1; j++){
-    if (arr[j] >arr[j+1]){
-        int temp=arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1] = temp;
-    }
-    }
-    }
-    cout <<"sorted array";
+using namespace std; 
+int main (){
+    int n=5; 
+    int arr[100]={1,2,3,4,5};
+    //sum of array
+    int sum=0; 
     for(int i=0; i<n; i++){
-        cout << arr[i]<< " ";
-        
+        sum += arr[i];
+    }
+    cout << " sum of array = "<< sum << endl ; 
+    //maximum of array 
+    int max=arr[0];
+    for(int i=1; i<n; i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    cout << " max element = "<< max<< endl; 
+    // minimum of array 
+    int min = arr[0];
+    for(int i=1; i<n; i++){
+        if( arr[i]<min){
+            min=arr[i]; 
+        }
+    }
+    cout<< " minimum of array = "<< min<< endl; 
+    //revers of array 
+    int start=0; int end =n-1; 
+    while (start<=end){
+        int temp =arr[start];
+        arr[start]=arr[end];
+        arr[end]= temp;
+        start++;
+        end--;
+
+    }
+    //print of revers of arrary
+    for(int i=0; i<n; i++){
+        cout<< arr[i]<< " ";
     }
     return 0; 
 }
