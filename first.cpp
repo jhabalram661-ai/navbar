bc70d361e15dfacf9bafd34ac0f9b69a481c59d6
kishan mandal <jhabalram661@gmail.com>
2026-01-26 14:31:32 +0530

    thi is comited code for patten 1

--- first.cpp
+++ first.cpp
@@ -39,5 +39,55 @@ int main (){
     for(int i=0; i<n; i++){
         cout<< arr[i]<< " ";
     }
-    return 0; 
+    // even and odd
+    int even =0; int odd=0; 
+    for(int i=0; i<n; i++){
+        if(arr[i]%2==0){
+            even++;
+        }else{
+            odd++;
+        }
+    }
+    cout << " how much even =  "<< even <<endl;
+    cout << " how much odd= " << odd << endl ;  
+    // print a even and odd 
+    for(int i=0; i<n; i++){
+        if(arr[i]%2==0){
+            cout << " even index= "<< i << " ";
+        }
+    }
+    for(int i=0; i<n; i++){
+        if(arr[i]%2!=0){
+            cout << " odd index = "<< i << " ";
+        }
+    }
+    
+    int positive =0; int negitive =0; 
+    int arr2[100]={1,-1,2,3,-5,-3};
+    int n2=6; 
+    for(int i=0; i<n2; i++){
+        if(arr2[i]>0){
+            positive++;
+        }else{
+            negitive++;
+        }
+    }
+    cout << " only positive number is = "<<positive; 
+    cout << " only negitive number is ="<< negitive ;
+
+    // print a positive and negitive 
+    cout <<" this is a positive number "<<endl; 
+    for(int i=0; i<n2; i++){
+        if(arr2[i]>0){
+            cout << i; 
+        }
+    }
+    cout <<" this is a negivtive number "<<endl; 
+    for(int i=0; i<n2; i++){
+        if(arr2[i]<0){
+            cout << i; 
+        }
+    }
+       
+       return 0; 
 }
